33cd892e18ef2121ad8d700d7fb987882991a970
akirasumeragi699-design <akirasumeragi699@gmail.com>
2025-07-30 16:36:19 +0800

    Update FAKE_JRE_MEMORY.h

--- bybylaucherjava/FAKE_JRE_MEMORY.h
+++ bybylaucherjava/FAKE_JRE_MEMORY.h
@@ -1,8 +1,21 @@
+#ifndef FAKE_JRE_MEMORY_H
+#define FAKE_JRE_MEMORY_H
+
 #include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct {
     void* (*malloc)(size_t size);
-    void (*free)(void* ptr);
+    void  (*free)(void* ptr);
 } JRE_MemoryAPI;
 
-JRE_MemoryAPI* JRE_GetMemoryAPI();
+JRE_MemoryAPI* JRE_GetMemoryAPI(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FAKE_JRE_MEMORY_H
