47135b45b939538b8cb225d41aac164e99d82b0f
akirasumeragi699-design <akirasumeragi699@gmail.com>
2025-07-30 16:35:39 +0800

    Update FAKE_JNI_LOADER.m

--- bybylaucherjava/FAKE_JNI_LOADER.m
+++ bybylaucherjava/FAKE_JNI_LOADER.m
@@ -1,45 +1,24 @@
 #import "DYNAMIC_LOADER.h"
 #import "FAKE_JRE_MEMORY.h"
+#import <sys/mman.h>
+#import <string.h>
 
-void JNI_LoadDynamicCode(const char* name, const void* code, size_t size) {
-    NSLog(@"[JNI] Loading dynamic code: %s", name);
-    
-    void* execMem = [DynamicLoader loadCode:code size:size];
-    if (execMem) {
-        // Gọi hàm đã nạp với tham số mẫu
-        void* args[] = { "DynamicCode" };
-        [DynamicLoader callFunction:execMem args:args];
+void* FakeJIT_Compile(const uint8_t* machineCode, size_t length) {
+    if (!machineCode || length == 0) return NULL;
+
+    void* mem = mmap(NULL, length, PROT_READ | PROT_WRITE | PROT_EXEC,
+                     MAP_ANON | MAP_PRIVATE, -1, 0);
+    if (mem == MAP_FAILED) return NULL;
+
+    memcpy(mem, machineCode, length);
+    return mem;
+}
+
+void JNI_SubmitBytecode(const char* name, const uint8_t* code, size_t length) {
+    if (!name || !code || length == 0) return;
+
+    void* compiled = FakeJIT_Compile(code, length);
+    if (compiled) {
+        [DynamicLoader registerLoadedFunction:compiled withName:name];
     }
 }
-
-// Ví dụ sử dụng:
-- (void)testDynamicLoad {
-    const unsigned char fakeCode[] = { 0x90, 0x90, 0xC3 }; // Code máy giả (NOP + RET)
-    JNI_LoadDynamicCode("fakeMethod", fakeCode, sizeof(fakeCode));
-}
-FAKE_JRE_MEMORY.c: #include "FAKE_JRE_MEMORY.h"
-#include <stdlib.h>
-
-static void* JRE_Malloc(size_t size) {
-    return malloc(size);
-}
-
-static void JRE_Free(void* ptr) {
-    free(ptr);
-}
-
-JRE_MemoryAPI* JRE_GetMemoryAPI() {
-    static JRE_MemoryAPI api = {
-        .malloc = &JRE_Malloc,
-        .free = &JRE_Free
-    };
-    return &api;
-}
-FAKE_JRE_MEMORY.h: #include <stddef.h>
-
-typedef struct {
-    void* (*malloc)(size_t size);
-    void (*free)(void* ptr);
-} JRE_MemoryAPI;
-
-JRE_MemoryAPI* JRE_GetMemoryAPI();
